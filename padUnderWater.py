1b83e89e2e1b85bc510d32a5480e408e897efa8f
guille <gdueasar@hawk.iit.educlearexit()(git config --global user.email gdueasar@hawk.iit.edu>
2015-07-09 14:54:37 -0500

    obtain the plot with the fit polynomials

--- padUnderWater.py
+++ padUnderWater.py
@@ -3,6 +3,7 @@ __author__ = 'moon'
 import numpy as np
 import cv2
 import time
+from matplotlib import pyplot as plt
 
 ##------------------------
 ## simple
@@ -12,7 +13,6 @@ import time
 ##----------------------
 ## object tracking
 cap = cv2.VideoCapture('../Images/light1.MOV')
-result=open('mask.txt','w')
 
 frameNum= 0
 points= []
@@ -22,6 +22,10 @@ while(1):
     # Take each frame
     _, frame = cap.read()
     frame= frame[450:1000, 300:1600]
+    frame= cv2.pyrDown(frame)
+    # frame= cv2.pyrDown(frame)
+    # frame= cv2.pyrDown(frame)
+
    # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -36,30 +40,36 @@ while(1):
 
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    imgArray= np.array(res)
+    imgArray= np.array(mask)
     points.append(np.transpose(np.nonzero(imgArray)))
-
-
-    # result.write('\n')
-    # result.write('\n')
-    # result.write(mask)
-
 
     cv2.imshow('mask',mask)
 
     end= time.time()
-    if end - start > 2:
+    if end - start > 3.5:
         break
 
     frameNum= frameNum + 1
-    k = cv2.waitKey(5) & 0xFF
+    k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
 
-##how to find hsv values to track?
-result.close()
-
-print points[0].shape
-
-
 cv2.destroyAllWindows()
+
+
+xs = np.arange(0,650,1)
+for i in range(frameNum/2,frameNum):
+    x= points[i][:,0]
+    y= points[i][:,1]
+
+    z= np.polyfit(x,y,3)
+    polynomial= np.poly1d(z)
+
+    ys= polynomial(xs)
+
+    plt.plot(xs,ys,'r')
+
+
+
+
+plt.show()
